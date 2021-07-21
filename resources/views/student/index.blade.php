04061bb77007730b19df9c6fc9d7e294dc5a87d0
JulzPerez <julietoperez@gmail.com>
2021-07-21 12:25:53 -0700

    july20-add11

--- resources/views/student/index.blade.php
+++ resources/views/student/index.blade.php
@@ -37,140 +37,171 @@
         </div>
     </div>
     @else 
-    <div class="row ">
-        
-        <!-- /.col -->
-        <div class="col-md-12">
-            <div class="card card-primary">
-                <div class="card-header">
-                    <h5> Student Information </h5>
+    <!-- ./row -->
+    <div class="row">
+          <div class="col-md-12">
+            <div class="card card-primary card-tabs">
+              <div class="card-header p-0 pt-1">
+                <ul class="nav nav-tabs" id="custom-tabs-one-tab" role="tablist">
+                  <li class="nav-item">
+                    <a class="nav-link active" id="custom-tabs-one-home-tab" data-toggle="pill" href="#custom-tabs-one-home" role="tab" aria-controls="custom-tabs-one-home" aria-selected="true">Personal Info</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" id="custom-tabs-one-profile-tab" data-toggle="pill" href="#custom-tabs-one-profile" role="tab" aria-controls="custom-tabs-one-profile" aria-selected="false">Family Info</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" id="custom-tabs-one-messages-tab" data-toggle="pill" href="#custom-tabs-one-messages" role="tab" aria-controls="custom-tabs-one-messages" aria-selected="false">Contact Info / Address</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" id="custom-tabs-one-settings-tab" data-toggle="pill" href="#custom-tabs-one-settings" role="tab" aria-controls="custom-tabs-one-settings" aria-selected="false">Educational Background</a>
+                  </li>
+                </ul>
+              </div>
+              <div class="card-body">
+                <div class="tab-content" id="custom-tabs-one-tabContent">
+                  <div class="tab-pane fade show active" id="custom-tabs-one-home" role="tabpanel" aria-labelledby="custom-tabs-one-home-tab">
+                    <table class="table table-bordered table-condensed">
+                        <tbody>
+                            <tr>
+                                <td class="text-muted" width="30%">ID No.</td>
+                                <td class="text-olive"> {{ ucfirst($student->id)}}</td>
+                            </tr>
+                            <tr>
+                                <td class="text-muted" width="30%">LRN</td>
+                                <td class="text-olive"> {{ ucfirst($student->LRN)}}</td>
+                            </tr>
+                            <tr>
+                                <td class="text-muted">First Name</td>
+                                <td class="text-olive"> {{ucfirst($student->first_name)}} </td>
+                            </tr>
+
+                            <tr>
+                                <td class="text-muted">Middle Name</td>
+                                <td class="text-olive"> {{ucfirst($student->middle_name)}} </td>
+                            </tr>
+                            <tr>
+                                <td class="text-muted">Last Name</td>
+                                <td class="text-olive"> {{ucfirst($student->last_name)}}</td>
+                            </tr>
+
+                            <tr>
+                                <td class="text-muted">Suffix/Name Extension</td>
+                                <td class="text-olive">{{ ucfirst($student->name_extension)}}</td>
+                            </tr>
+
+                            <tr>
+                                <td class="text-muted">Sex</td>
+                                <td class="text-olive">{{ ucfirst($student->sex)}}</td>
+                            </tr>
+
+                            <tr>
+                                <td class="text-muted">Contact Number</td>
+                                <td class="text-olive">{{ ucfirst($student->contact_no)}}</td>
+                            </tr>
+                            
+                            <tr>
+                                <td class="text-muted">Religion</td>
+                                <td class="text-olive">{{ ucfirst($student->religion)}}</td>
+                            </tr>
+                            <tr>
+                                <td class="text-muted">Citizenship</td>
+                                <td class="text-olive">{{ ucfirst($student->citizenship)}}</td>
+                            </tr>
+
+                            <tr>
+                                <td class="text-muted">Date of Birth</td>
+                                <td class="text-olive"> {{ ucfirst($student->birthday)}}</td>
+                            </tr>
+                            <tr>
+                                <td class="text-muted">Place of Birth</td>
+                                <td class="text-olive"> {{ ucfirst($student->birthplace)}}</td>
+                            </tr>
+                            <tr>
+                                <td class="text-muted">No. of Siblings</td>
+                                <td class="text-olive"> {{ ucfirst($student->no_siblings)}}</td>
+                            </tr>
+                            <tr>
+                                <td class="text-muted">Birth Order</td>
+                                <td class="text-olive"> {{ ucfirst($student->birth_order)}}</td>
+                            </tr>
+
+                            
+                            <tr>
+                                <td class="text-muted">Ethnicity</td>
+                                <td class="text-olive"> </td>
+                            </tr>
+
+                            <tr>
+                                <td class="text-muted">Mother Tounge</td>
+                                <td class="text-olive"> </td>
+                            </tr>
+                            
+                        </tbody>                          
+                    </table>
+                  </div>
+                  <div class="tab-pane fade" id="custom-tabs-one-profile" role="tabpanel" aria-labelledby="custom-tabs-one-profile-tab">
+                  <table class="table table-bordered table-condensed">
+                        <tbody>
+                            
+
+                            <tr>
+                                <td class="text-muted">Father</td>
+                                <td class="text-olive"> </td>
+                            </tr>
+                            <tr>
+                                <td class="text-muted">Father Occupation</td>
+                                <td class="text-olive"></td>
+                            </tr>
+                            <tr>
+                                <td class="text-muted">Mother</td>
+                                <td class="text-olive"> </td>
+                            </tr>
+
+                            <tr>
+                                <td class="text-muted">Mother Occupation</td>
+                                <td class="text-olive"> </td>
+                            </tr>
+
+                            
+                            
+                        </tbody>                          
+                    </table>
+
+                  </div>
+                  <div class="tab-pane fade" id="custom-tabs-one-messages" role="tabpanel" aria-labelledby="custom-tabs-one-messages-tab">
+                  <table class="table table-bordered table-condensed">
+                        <tbody>
+                            
+
+                            <tr>
+                                <td class="text-muted">Purok</td>
+                                <td class="text-olive"> {{ ucfirst($student->purok)}}</td>
+                            </tr>
+
+                            <tr>
+                                <td class="text-muted">Municipality</td>
+                                <td class="text-olive"> </td>
+                            </tr>
+
+                            <tr>
+                                <td class="text-muted">Province</td>
+                                <td class="text-olive"> </td>
+                            </tr>
+
+                            
+                        </tbody>                          
+                    </table>
+
+                  </div>
+                  <div class="tab-pane fade" id="custom-tabs-one-settings" role="tabpanel" aria-labelledby="custom-tabs-one-settings-tab">
+                  </div>
                 </div>
-            
-            <div class="card-body">
-                <table class="table table-bordered table-condensed">
-                    <tbody>
-                        <tr>
-                            <td class="text-muted" width="30%">ID No.</td>
-                            <td class="text-olive"> {{ ucfirst($student->id)}}</td>
-                        </tr>
-                        <tr>
-                            <td class="text-muted" width="30%">LRN</td>
-                            <td class="text-olive"> {{ ucfirst($student->LRN)}}</td>
-                        </tr>
-                        <tr>
-                            <td class="text-muted">First Name</td>
-                            <td class="text-olive"> {{ucfirst($student->first_name)}} </td>
-                        </tr>
-
-                        <tr>
-                            <td class="text-muted">Middle Name</td>
-                            <td class="text-olive"> {{ucfirst($student->middle_name)}} </td>
-                        </tr>
-                        <tr>
-                            <td class="text-muted">Last Name</td>
-                            <td class="text-olive"> {{ucfirst($student->last_name)}}</td>
-                        </tr>
-
-                        <tr>
-                            <td class="text-muted">Suffix/Name Extension</td>
-                            <td class="text-olive">{{ ucfirst($student->name_extension)}}</td>
-                        </tr>
-
-                        <tr>
-                            <td class="text-muted">Sex</td>
-                            <td class="text-olive">{{ ucfirst($student->sex)}}</td>
-                        </tr>
-
-                        <tr>
-                            <td class="text-muted">Contact Number</td>
-                            <td class="text-olive">{{ ucfirst($student->contact_no)}}</td>
-                        </tr>
-                        
-                        <tr>
-                            <td class="text-muted">Religion</td>
-                            <td class="text-olive">{{ ucfirst($student->religion)}}</td>
-                        </tr>
-                        <tr>
-                            <td class="text-muted">Citizenship</td>
-                            <td class="text-olive">{{ ucfirst($student->citizenship)}}</td>
-                        </tr>
-
-                        <tr>
-                            <td class="text-muted">Date of Birth</td>
-                            <td class="text-olive"> {{ ucfirst($student->birthday)}}</td>
-                        </tr>
-                        <tr>
-                            <td class="text-muted">Place of Birth</td>
-                            <td class="text-olive"> {{ ucfirst($student->birthplace)}}</td>
-                        </tr>
-                        <tr>
-                            <td class="text-muted">No. of Siblings</td>
-                            <td class="text-olive"> {{ ucfirst($student->no_siblings)}}</td>
-                        </tr>
-                        <tr>
-                            <td class="text-muted">Birth Order</td>
-                            <td class="text-olive"> {{ ucfirst($student->birth_order)}}</td>
-                        </tr>
-
-                        <tr>
-                            <td class="text-muted">Purok</td>
-                            <td class="text-olive"> {{ ucfirst($student->purok)}}</td>
-                        </tr>
-
-                        <tr>
-                            <td class="text-muted">Municipality</td>
-                            <td class="text-olive"> </td>
-                        </tr>
-
-                        <tr>
-                            <td class="text-muted">Province</td>
-                            <td class="text-olive"> </td>
-                        </tr>
-
-                        <tr>
-                            <td class="text-muted">Father</td>
-                            <td class="text-olive"> </td>
-                        </tr>
-                        <tr>
-                            <td class="text-muted">Father Occupation</td>
-                            <td class="text-olive"></td>
-                        </tr>
-                        <tr>
-                            <td class="text-muted">Mother</td>
-                            <td class="text-olive"> </td>
-                        </tr>
-
-                        <tr>
-                            <td class="text-muted">Mother Occupation</td>
-                            <td class="text-olive"> </td>
-                        </tr>
-
-                        <tr>
-                            <td class="text-muted">Modality</td>
-                            <td class="text-olive"> </td>
-                        </tr>
-
-                        <tr>
-                            <td class="text-muted">Ethnicity</td>
-                            <td class="text-olive"> </td>
-                        </tr>
-
-                        <tr>
-                            <td class="text-muted">Mother Tounge</td>
-                            <td class="text-olive"> </td>
-                        </tr>
-                        
-                    </tbody>                          
-                </table>
-                
-                
-            </div><!-- /.card-body -->
+              </div>
+              <!-- /.card -->
             </div>
-            <!-- /.nav-tabs-custom -->
-        </div>
-        <!-- /.col -->
+          </div>
     </div>
-
+    
         
 
     @endif
