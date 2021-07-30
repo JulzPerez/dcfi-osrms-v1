9a008d2417c166244f2327a047a4e68cca2ec464
JulzPerez <julietoperez@gmail.com>
2021-07-30 01:04:49 -0700

    fixing errros and adding functions-change1

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -204,7 +204,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
 
           @can('isBFO')
           <li class="nav-item">
-            <a href="{{ route('account.getPayments') }}" class="nav-link">
+            <a href="{{ route('searchForm') }}" class="nav-link">
             <i class="nav-icon  fas fa-file-invoice-dollar"></i>
               <p>
                 View Payments          
