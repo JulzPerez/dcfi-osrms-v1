bd68b6367c37f794fe12a6009338a5201cd0326e
JulzPerez <julietoperez@gmail.com>
2021-11-23 15:04:38 -0800

    nov23-changes

--- resources/views/grades/SHSgrades.blade.php
+++ resources/views/grades/SHSgrades.blade.php
@@ -20,95 +20,102 @@
         </div>
     </div>
 
-    <div class="row">
-        <div class="col-md-12">
-            <div class="card">
-                <div class="card-body">
-                <h5 class="float-left"> Grades: Learning Areas</h5>
-                        <table class="table table-hover table-bordered table-striped">
-                            <thead>
-                                
-                                <tr>
-                                    <th style="width:5%">#</th>
-                                    <th style="width:15%"> Code</th>   
-                                    <th style="width:30%"> Description</th> 
-                                    <th style="width:10%"> 1st Quarter</th>   
-                                    <th style="width:10%"> 2nd Quarter</th>
+    @if($academics[0]->Locking === null)
+        <div class="row">
+            <div class="container">
+                <p style="color:red"> Viewing of grades is not yet allowed. </p>
+            </div>
+        </div>
+    @else
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-body">
+                    <h5 class="float-left"> Grades: Learning Areas</h5>
+                            <table class="table table-hover table-bordered table-striped">
+                                <thead>
                                     
-                                    <th style="width:10%"> Final</th>
-                                                
-                                </tr>
-                            </thead>
-                            <?php $count = 0; ?>
-                                <tbody style="line-height: 0.75">
-                                   
-                                    @foreach($academics as $academic)
                                     <tr>
-                                        <td>{{++$count}}</td>
-                                        <td>{{ucwords($academic->Code)}}</td>
-                                        <td>{{ucwords($academic->Description)}}</td>
-                                        <td>{{$academic->first_grading}}</td>
-                                        <td>{{$academic->second_grading}}</td>
-                                       
-                                        <td>{{$academic->final_grading}}</td>
-                                      
+                                        <th style="width:5%">#</th>
+                                        <th style="width:15%"> Code</th>   
+                                        <th style="width:30%"> Description</th> 
+                                        <th style="width:10%"> 1st Quarter</th>   
+                                        <th style="width:10%"> 2nd Quarter</th>
                                         
-                                    </tr>                            
-                                    @endforeach
-                                </tbody>
-                        
-                        </table>
+                                        <th style="width:10%"> Final</th>
+                                                    
+                                    </tr>
+                                </thead>
+                                <?php $count = 0; ?>
+                                    <tbody style="line-height: 0.75">
+                                    
+                                        @foreach($academics as $academic)
+                                        <tr>
+                                            <td>{{++$count}}</td>
+                                            <td>{{ucwords($academic->Code)}}</td>
+                                            <td>{{ucwords($academic->Description)}}</td>
+                                            <td>{{$academic->first_grading}}</td>
+                                            <td>{{$academic->second_grading}}</td>
+                                        
+                                            <td>{{$academic->final_grading}}</td>
+                                        
+                                            
+                                        </tr>                            
+                                        @endforeach
+                                    </tbody>
+                            
+                            </table>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
 
-    
+        
 
-    <div class="row">
-        <div class="col-md-12">
-            <div class="card">
-                <div class="card-body">
-                <h5 class="float-left"> Grades: Character Building Activities</h5>
-                        <table class="table table-hover table-bordered table-striped">
-                            <thead>
-                                
-                                <tr>
-                                    <th style="width:5%">#</th>
-                                    <th style="width:15%"> Code</th>   
-                                    <th style="width:30%"> Description</th> 
-                                    <th style="width:10%"> 1st Quarter </th>   
-                                    <th style="width:10%"> 2nd Quarter</th>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-body">
+                    <h5 class="float-left"> Grades: Character Building Activities</h5>
+                            <table class="table table-hover table-bordered table-striped">
+                                <thead>
                                     
-                                    <th style="width:10%"> Final</th>
-                                                
-                                </tr>
-                            </thead>
-                            <?php $count = 0; ?>
-                                <tbody style="line-height: 0.75">
-                                   
-                                    @foreach($characters as $character)
                                     <tr>
-                                        <td>{{++$count}}</td>
-                                        <td>{{ucwords($character->Code)}}</td>
-                                        <td>{{ucwords($character->Description)}}</td>
-                                        <td>{{$character->first_grading}}</td>
-                                        <td>{{$character->second_grading}}</td>
+                                        <th style="width:5%">#</th>
+                                        <th style="width:15%"> Code</th>   
+                                        <th style="width:30%"> Description</th> 
+                                        <th style="width:10%"> 1st Quarter </th>   
+                                        <th style="width:10%"> 2nd Quarter</th>
                                         
-                                        <td>{{$character->final_grading}}</td>
-                                      
+                                        <th style="width:10%"> Final</th>
+                                                    
+                                    </tr>
+                                </thead>
+                                <?php $count = 0; ?>
+                                    <tbody style="line-height: 0.75">
+                                    
+                                        @foreach($characters as $character)
+                                        <tr>
+                                            <td>{{++$count}}</td>
+                                            <td>{{ucwords($character->Code)}}</td>
+                                            <td>{{ucwords($character->Description)}}</td>
+                                            <td>{{$character->first_grading}}</td>
+                                            <td>{{$character->second_grading}}</td>
+                                            
+                                            <td>{{$character->final_grading}}</td>
                                         
-                                    </tr>                            
-                                    @endforeach
-                                </tbody>
-                        
-                        </table>
+                                            
+                                        </tr>                            
+                                        @endforeach
+                                    </tbody>
+                            
+                            </table>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
 
-   
+    @endif
 
 
         
--- resources/views/grades/index.blade.php
+++ resources/views/grades/index.blade.php
@@ -20,140 +20,149 @@
         </div>
     </div>
 
-    <div class="row">
-        <div class="col-md-12">
-            <div class="card">
-                <div class="card-body">
-                <h5 class="float-left"> Grades: Learning Areas</h5>
-                        <table class="table table-hover table-bordered table-striped">
-                            <thead>
-                                
-                                <tr>
-                                    <th style="width:5%">#</th>
-                                    <th style="width:15%"> Code</th>   
-                                    <th style="width:30%"> Description</th> 
-                                    <th style="width:10%"> 1st </th>   
-                                    <th style="width:10%"> 2nd</th>
-                                    <th style="width:10%"> 3rd</th>
-                                    <th style="width:10%"> 4th</th>
-                                    <th style="width:10%"> Final</th>
-                                                
-                                </tr>
-                            </thead>
-                            <?php $count = 0; ?>
-                                <tbody style="line-height: 0.75">
-                                   
-                                    @foreach($academics as $academic)
+    @if($academics[0]->Locking === null)
+        <div class="row">
+            <div class="container">
+                <p style="color:red"> Viewing of grades is not yet allowed. </p>
+            </div>
+        </div>
+    @else
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-body">
+                    <h5 class="float-left"> Grades: Learning Areas</h5>
+                            <table class="table table-hover table-bordered table-striped">
+                                <thead>
+                                    
                                     <tr>
-                                        <td>{{++$count}}</td>
-                                        <td>{{ucwords($academic->Code)}}</td>
-                                        <td>{{ucwords($academic->Description)}}</td>
-                                        <td>{{$academic->first_grading}}</td>
-                                        <td>{{$academic->second_grading}}</td>
-                                        <td>{{$academic->third_grading}}</td>
-                                        <td>{{$academic->fourth_grading}}</td>
-                                        <td>{{$academic->final_grading}}</td>
-                                      
+                                        <th style="width:5%">#</th>
+                                        <th style="width:15%"> Code</th>   
+                                        <th style="width:30%"> Description</th> 
+                                        <th style="width:10%"> 1st </th>   
+                                        <th style="width:10%"> 2nd</th>
+                                        <th style="width:10%"> 3rd</th>
+                                        <th style="width:10%"> 4th</th>
+                                        <th style="width:10%"> Final</th>
+                                                    
+                                    </tr>
+                                </thead>
+                                <?php $count = 0; ?>
+                                    <tbody style="line-height: 0.75">
+                                    
+                                        @foreach($academics as $academic)
+                                        <tr>
+                                            <td>{{++$count}}</td>
+                                            <td>{{ucwords($academic->Code)}}</td>
+                                            <td>{{ucwords($academic->Description)}}</td>
+                                            <td>{{$academic->first_grading}}</td>
+                                            <td>{{$academic->second_grading}}</td>
+                                            <td>{{$academic->third_grading}}</td>
+                                            <td>{{$academic->fourth_grading}}</td>
+                                            <td>{{$academic->final_grading}}</td>
                                         
-                                    </tr>                            
-                                    @endforeach
-                                </tbody>
-                        
-                        </table>
+                                        </tr>                            
+                                        @endforeach
+                                    </tbody>
+                            
+                            </table>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
 
-    <div class="row">
-        <div class="col-md-12">
-            <div class="card">
-                <div class="card-body">
-                <h5 class="float-left"> Grades: Non-Academic Activities</h5>
-                        <table class="table table-hover table-bordered table-striped">
-                            <thead>
-                                
-                                <tr>
-                                    <th style="width:5%">#</th>
-                                    <th style="width:15%"> Code</th>   
-                                    <th style="width:30%"> Description</th> 
-                                    <th style="width:10%"> 1st </th>   
-                                    <th style="width:10%"> 2nd</th>
-                                    <th style="width:10%"> 3rd</th>
-                                    <th style="width:10%"> 4th</th>
-                                    <th style="width:10%"> Final</th>
-                                                
-                                </tr>
-                            </thead>
-                            <?php $count = 0; ?>
-                                <tbody style="line-height: 0.75">
-                                   
-                                    @foreach($non_academics as $non_academic)
-                                    <tr>
-                                        <td>{{++$count}}</td>
-                                        <td>{{ucwords($non_academic->Code)}}</td>
-                                        <td>{{ucwords($non_academic->Description)}}</td>
-                                        <td>{{$non_academic->first_grading}}</td>
-                                        <td>{{$non_academic->second_grading}}</td>
-                                        <td>{{$non_academic->third_grading}}</td>
-                                        <td>{{$non_academic->fourth_grading}}</td>
-                                        <td>{{$non_academic->final_grading}}</td>
-                                      
-                                        
-                                    </tr>                            
-                                    @endforeach
-                                </tbody>
-                        
-                        </table>
-                </div>
-            </div>
-        </div>
-    </div>
+        
 
-    <div class="row">
-        <div class="col-md-12">
-            <div class="card">
-                <div class="card-body">
-                <h5 class="float-left"> Grades: Character Building Activities</h5>
-                        <table class="table table-hover table-bordered table-striped">
-                            <thead>
-                                
-                                <tr>
-                                    <th style="width:5%">#</th>
-                                    <th style="width:15%"> Code</th>   
-                                    <th style="width:30%"> Description</th> 
-                                    <th style="width:10%"> 1st </th>   
-                                    <th style="width:10%"> 2nd</th>
-                                    <th style="width:10%"> 3rd</th>
-                                    <th style="width:10%"> 4th</th>
-                                    <th style="width:10%"> Final</th>
-                                                
-                                </tr>
-                            </thead>
-                            <?php $count = 0; ?>
-                                <tbody style="line-height: 0.75">
-                                   
-                                    @foreach($characters as $character)
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-body">
+                    <h5 class="float-left"> Grades: Non-Academic Activities</h5>
+                            <table class="table table-hover table-bordered table-striped">
+                                <thead>
+                                    
                                     <tr>
-                                        <td>{{++$count}}</td>
-                                        <td>{{ucwords($character->Code)}}</td>
-                                        <td>{{ucwords($character->Description)}}</td>
-                                        <td>{{$character->first_grading}}</td>
-                                        <td>{{$character->second_grading}}</td>
-                                        <td>{{$character->third_grading}}</td>
-                                        <td>{{$character->fourth_grading}}</td>
-                                        <td>{{$character->final_grading}}</td>
-                                      
+                                        <th style="width:5%">#</th>
+                                        <th style="width:15%"> Code</th>   
+                                        <th style="width:30%"> Description</th> 
+                                        <th style="width:10%"> 1st </th>   
+                                        <th style="width:10%"> 2nd</th>
+                                        <th style="width:10%"> 3rd</th>
+                                        <th style="width:10%"> 4th</th>
+                                        <th style="width:10%"> Final</th>
+                                                    
+                                    </tr>
+                                </thead>
+                                <?php $count = 0; ?>
+                                    <tbody style="line-height: 0.75">
+                                    
+                                        @foreach($non_academics as $non_academic)
+                                        <tr>
+                                            <td>{{++$count}}</td>
+                                            <td>{{ucwords($non_academic->Code)}}</td>
+                                            <td>{{ucwords($non_academic->Description)}}</td>
+                                            <td>{{$non_academic->first_grading}}</td>
+                                            <td>{{$non_academic->second_grading}}</td>
+                                            <td>{{$non_academic->third_grading}}</td>
+                                            <td>{{$non_academic->fourth_grading}}</td>
+                                            <td>{{$non_academic->final_grading}}</td>
                                         
-                                    </tr>                            
-                                    @endforeach
-                                </tbody>
-                        
-                        </table>
+                                            
+                                        </tr>                            
+                                        @endforeach
+                                    </tbody>
+                            
+                            </table>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-body">
+                    <h5 class="float-left"> Grades: Character Building Activities</h5>
+                            <table class="table table-hover table-bordered table-striped">
+                                <thead>
+                                    
+                                    <tr>
+                                        <th style="width:5%">#</th>
+                                        <th style="width:15%"> Code</th>   
+                                        <th style="width:30%"> Description</th> 
+                                        <th style="width:10%"> 1st </th>   
+                                        <th style="width:10%"> 2nd</th>
+                                        <th style="width:10%"> 3rd</th>
+                                        <th style="width:10%"> 4th</th>
+                                        <th style="width:10%"> Final</th>
+                                                    
+                                    </tr>
+                                </thead>
+                                <?php $count = 0; ?>
+                                    <tbody style="line-height: 0.75">
+                                    
+                                        @foreach($characters as $character)
+                                        <tr>
+                                            <td>{{++$count}}</td>
+                                            <td>{{ucwords($character->Code)}}</td>
+                                            <td>{{ucwords($character->Description)}}</td>
+                                            <td>{{$character->first_grading}}</td>
+                                            <td>{{$character->second_grading}}</td>
+                                            <td>{{$character->third_grading}}</td>
+                                            <td>{{$character->fourth_grading}}</td>
+                                            <td>{{$character->final_grading}}</td>
+                                        
+                                            
+                                        </tr>                            
+                                        @endforeach
+                                    </tbody>
+                            
+                            </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endif
 
    
 
